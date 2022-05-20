e8d7ae9d665aaafb2abd0e7278682ba2cbbf9c73
ErikUTA <68131681+ErikUTA@users.noreply.github.com>
2022-05-19 21:28:46 -0500

    solving new bugs

--- src/components/elements/navbar/navbar.jsx
+++ src/components/elements/navbar/navbar.jsx
@@ -42,9 +42,9 @@ export default function SearchAppBar() {
                     <svg class="ast-mobile-svg ast-menu2-svg" fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="28" viewBox="0 0 24 28"><path d="M24 21v2c0 0.547-0.453 1-1 1h-22c-0.547 0-1-0.453-1-1v-2c0-0.547 0.453-1 1-1h22c0.547 0 1 0.453 1 1zM24 13v2c0 0.547-0.453 1-1 1h-22c-0.547 0-1-0.453-1-1v-2c0-0.547 0.453-1 1-1h22c0.547 0 1 0.453 1 1zM24 5v2c0 0.547-0.453 1-1 1h-22c-0.547 0-1-0.453-1-1v-2c0-0.547 0.453-1 1-1h22c0.547 0 1 0.453 1 1z"></path></svg>
                   </button>
                 </div>
-                {/* New changes on feature branch */}
+                {/* solving bugs */}
 
-                <Button>Enviar</Button>New data1
+                <Button className="data">Enviar</Button>
 
                 {/*  */}
 
